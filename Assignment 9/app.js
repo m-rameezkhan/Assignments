3dc407fb25c1ebf59deded494a5ac5f73ca9faa6
Muhammad Rameez Khan <mrameezkhan1785@gmail.com>
2025-03-13 09:17:35 +0500

    Done 6 Questions

--- Assignment 9/app.js
+++ Assignment 9/app.js
@@ -19,14 +19,57 @@
 // yearCheaker(prompt("Enter a Year"))
 
 // Question 3
-function areaOfTriangle(a, b, c) {
-    let s = calculateS(a, b, c)
-    let area = s*(s - a)*(s - b)*(s - c)
-    area = Math.sqrt(area)
-    return area
+// function areaOfTriangle(a, b, c) {
+//     let s = calculateS(a, b, c)
+//     let area = s*(s - a)*(s - b)*(s - c)
+//     area = Math.sqrt(area)
+//     return area
+// }
+// function calculateS(a, b, c) {
+//     let s = (a + b + c) / 2
+//     return s
+// }
+// console.log(areaOfTriangle(4, 5, 7))
+
+// Question 4
+// function main() {
+//     let marks1 = +prompt("Enter the marks of Math")
+//     let marks2 = +prompt("Enter the marks of Computer")
+//     let marks3 = +prompt("Enter the marks of English")
+
+//     let percentage = calcPercentage(marks1, marks2, marks3)
+//     let average = calcAverage(marks1, marks2, marks3)
+//     console.log("Percentage: ", percentage)
+//     console.log("Average: ", average)
+// }
+// function calcPercentage(a, b, c){
+//     let total = 300
+//     let obtained = a + b + c
+//     let percentage = obtained / total * 100
+//     return percentage
+// }
+// function calcAverage(a, b, c) {
+//     let sum = a + b + c
+//     let average = sum / 3
+//     return average
+// }
+// main()
+
+// Question 5
+// function indexOfStr(str, char) {
+//     for (let i = 0; i < str.length; i++) {
+//         if (str[i] == char) {
+//             console.log(i)
+//             return i
+//         }
+//     }
+//     console.log("Character not found")
+// }
+// indexOfStr("Rameez", "o")
+
+// Question 6
+function delVowels(str) {
+    return str.replace(/[aeiouAEIOU]/g, '');
 }
-function calculateS(a, b, c) {
-    let s = (a + b + c) / 2
-    return s
-}
-console.log(areaOfTriangle(4, 5, 7))
+console.log(delVowels("Hello World"))
+
